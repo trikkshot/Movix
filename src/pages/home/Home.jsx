c055c4d6fe7aa8456f606861a080a4d15ba3eab0
Ankitgupta <122004897+trikkshot@users.noreply.github.com>
2023-03-02 11:53:33 +0530

    First commit
    Date: 3/3/23

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import "./style.scss";
+
+import HeroBanner from './heroBanner/HeroBanner';
+import Trending from './trending/Trending';
+import Popular from './popular/Popular';
+import TopRated from './topRated/TopRated';
+
+HeroBanner
+
+const Home = () => {
+  return <div className='homePage'>
+    <HeroBanner/>
+    <Trending />
+    <Popular />
+    <TopRated />
+    <div style={{ height: 1000 }}></div>
+  </div>
+  
+}
+
+export default Home;
